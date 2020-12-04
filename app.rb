a13fac5ccb39e7d6a2ed2b283ce798133c8b3791
julietaArgollas <julieta.c.argollas@gmail.com>
2020-12-03 22:54:51 -0400

    comentario

--- app.rb
+++ app.rb
@@ -5,10 +5,9 @@ require './lib/fizzbuzz.rb'
 get '/' do
     erb :home
 end
-
+#es la parte de convertir.
 post '/convertir' do
     @num=params[:Num].to_i
     @fizzbuzznum= FizzBuzz.new().convertir(@num)
-    
     erb :convertir
 end
